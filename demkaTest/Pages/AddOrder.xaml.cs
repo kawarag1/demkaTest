18e9c3f4b2ef8ce5dc95b49450ec2d8d17b1b6e8
kawaragi <dima.kretinin.05@mail.ru>
2026-06-17 23:51:37 +0700

    123

---
+++ demkaTest/Pages/AddOrder.xaml.cs
@@ -0,0 +1,57 @@
+﻿using demkaTest.Models;
+using demkaTest.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace demkaTest.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddOrder.xaml
+    /// </summary>
+    public partial class AddOrder : Page
+    {
+        public AddOrder()
+        {
+            InitializeComponent();
+            InitializeAdresses();
+            InitializeStatuses();
+        }
+
+        private void InitializeAdresses()
+        {
+            var context = Helper.GetContext();
+            AddressBox.ItemsSource = context.PickUpPoints.ToList();
+        }
+
+        private void InitializeStatuses()
+        {
+            var context = Helper.GetContext();
+            Statuses.ItemsSource = context.Statuses.ToList();
+        }
+        private void AddOrderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var context = Helper.GetContext();
+            Orders order = new Orders();
+            order.CreateDate = CreateDate.SelectedDate.Value;
+            order.DeliveryDate = DeliveryDate.SelectedDate.Value;
+            order.Code = 111;
+            order.UserID = UserStatic.user.id;
+            order.PickUpPointID = AddressBox.SelectedIndex + 1;
+            order.StatusID = Statuses.SelectedIndex + 1;
+            context.Orders.Add(order);
+            context.SaveChanges();
+        }
+    }
+}
--- demkaTest/Pages/OrdersPage.xaml.cs
+++ demkaTest/Pages/OrdersPage.xaml.cs
@@ -31,10 +31,41 @@ namespace demkaTest.Pages
         
         private void InitializeOrders()
         {
+            if (UserStatic.user.Roles.name == "Администратор")
+            {
+                AdminPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                AdminPanel.Visibility = Visibility.Hidden;
+            }
             var context = Helper.GetContext();
             var orders = context.Orders.ToList();
             orders_ = orders;
             LViewOrders.ItemsSource = orders_;
+        }
+
+        private void LViewOrders_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var order = LViewOrders.SelectedItem as Orders;
+            NavigationService.Navigate(new EditOrder(order));
+        }
+
+        private void AddOrder_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddOrder());
+        }
+
+        private void DeleteOrder_Click(object sender, RoutedEventArgs e)
+        {
+            var context = Helper.GetContext();
+            var order = LViewOrders.SelectedItem as Orders;
+            var contents = context.OrderContents.Where(x => x.orderID == order.id).ToList();
+            context.OrderContents.RemoveRange(contents);
+            context.Orders.Remove(order);
+            orders_.Remove(order);
+            LViewOrders.ItemsSource = orders_;
+            context.SaveChanges();
         }
     }
 }
